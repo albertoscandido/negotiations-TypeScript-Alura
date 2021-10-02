74b7109c2e89d4436269c90c349b72a2fc5e55bb
Alberto Candido <albertocandido98@gmail.com>
2021-10-02 15:45:57 -0300

    Simplifica classe Negociacao passando paramentros direto no construtor sem declarar essas propriedades anteriormente

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,13 +1,22 @@
 export class Negociacao {
-  private _data: Date;
-  private _quantidade: number;
-  private _valor: number;
+  // método comum
+  // private _data: Date;
+  // private _quantidade: number;
+  // private _valor: number;
 
-  constructor(data: Date, quantidade: number, valor: number) {
-    this._data = data;
-    this._quantidade = quantidade;
-    this._valor = valor;
-  }
+  // constructor(data: Date, quantidade: number, valor: number) {
+  //   this._data = data;
+  //   this._quantidade = quantidade;
+  //   this._valor = valor;
+  // }
+
+  // método mais simples usando o TypeScript - como o construtor recebe as propriedades que nosso objeto precisa
+  // podemos declarar elas no próprio parametrô dele 
+  constructor(
+    private _data: Date,
+    private _quantidade: number,
+    private _valor: number
+  ) {}
 
   get data(): Date { return this._data; }
   get quantidade(): number { return this._quantidade; }
--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -1,8 +1,19 @@
 export class Negociacao {
-    constructor(data, quantidade, valor) {
-        this._data = data;
-        this._quantidade = quantidade;
-        this._valor = valor;
+    // método comum
+    // private _data: Date;
+    // private _quantidade: number;
+    // private _valor: number;
+    // constructor(data: Date, quantidade: number, valor: number) {
+    //   this._data = data;
+    //   this._quantidade = quantidade;
+    //   this._valor = valor;
+    // }
+    // método mais simples usando o TypeScript - como o construtor recebe as propriedades que nosso objeto precisa
+    // podemos declarar elas no próprio parametrô dele 
+    constructor(_data, _quantidade, _valor) {
+        this._data = _data;
+        this._quantidade = _quantidade;
+        this._valor = _valor;
     }
     get data() { return this._data; }
     get quantidade() { return this._quantidade; }
