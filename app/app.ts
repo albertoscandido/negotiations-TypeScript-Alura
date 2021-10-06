b7a10aed109b04ab32707f011b05e6d00588061d
Alberto Candido <albertocandido98@gmail.com>
2021-10-06 17:00:26 -0300

    Inicia criaçao de classe negociacoes-view que ira atualizar uma tabela na pagina a cada negociaçao adicionada

--- app/app.ts
+++ app/app.ts
@@ -1,6 +1,7 @@
 // import { Negociacao } from './models/negociacao.js';
 
 import { NegociacaoController } from "./controllers/negociacao-controller.js";
+import { NegociacoesView } from "./views/negociacoes-view.js";
 
 // const negociacao = new Negociacao(new Date(), 10, 100);
 // console.log(negociacao.volume);
@@ -13,3 +14,6 @@ form.addEventListener('submit', (e: Event) => {
 
   controller.adiciona();
 });
+
+// const negociacoesView = new NegociacoesView();
+// const template = negociacoesView.template();
